d2412597ae4529113b8eafd50edf256b43905180
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-03-11 19:27:57 +0800

    Update home.jsx

--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -26,3 +26,14 @@ function Home() {
 }
 
 export default Home
+	
+<div class="pagelinks">    
+      </div>
+    </div>
+    <div class="information">
+      <h2><u>FOLLOW ME ON FACEBOOK</u></h2>
+      <h3>       👇       </h3>
+      <a href="https://www.facebook.com/profile.php?id=100069572806122" target="_blank">ANIMXER</a>
+      
+      <h3>       🇵🇭      </h3>
+      <h3>       🔥      </h3>
